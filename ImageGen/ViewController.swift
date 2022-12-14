0831a850eeebec23b09e7a611ff7831d4e700425
Igor Fernandes <igormedfer@gmail.com>
2022-12-14 16:04:07 -0300

    first commit

---
+++ ImageGen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGen
+//
+//  Created by Igor Fernandes on 14/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
